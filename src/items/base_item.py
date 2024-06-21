19a5e00028e0d27a41a382f3ce74cc9f090b0ef6
jootwaifu <myrainbowloom128@gmail.com>
2024-06-22 05:37:59 +0700

    УСНУЛА БЛЯТЬ НУ КАК  ТАК

--- src/items/base_item.py
+++ src/items/base_item.py
@@ -1,5 +1,27 @@
+import pygame
+from src.utils import get_mask_rect
+
 class BaseItem():
-    def __init__(self):
+    def __init__(self, game, room, name, object_type, size, player, position, damage, protection):
+        self.game = game
+        self.room = room
+        self.name = name
+        self.object_type = object_type
+        self.size = size
+        self.player = player
+        #fix it
+        self.image = None
+        self.path = f'./assets/objects/{self.name}'
+        self.load_image()
+        self.rect = self.image.get_rect()
+        self.hitbox = get_mask_rect(self.image, *self.rect.topleft)
+        #^^^^^^
+        if position:
+            self.rect.x, self.rect.y = position[0], position[1]
+        self.damage = damage
+        self.protection = protection
+        self.interaction = False
+        self.dropped = False
         pass
     
     def use(self):
